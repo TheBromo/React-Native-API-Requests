a3add6beaec232ada0d46136199fcdf5b64db54e
Manuel Strenge <manuel.strenge@hotmail.com>
2019-04-15 09:16:19 +0200

    Fixed some Requests and added error throws

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Requests from './requests.js'
-import {FlatList, ActivityIndicator, Text, View } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View } from 'react-native';
 
 export default class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={ isLoading: true}
+    this.state = { isLoading: true }
   }
 
-  componentDidMount(){
-    Requests.then(data => {
+  componentDidMount() {
+    Requests.loginAsUser("","").then(data => {
       this.setState({
         isLoading: false,
         dataSource: data.succes,
       });
-    });
+    }).catch(error => console.log('Error:', error));
+
     /*
     Requests.postData().then(movies => {
       this.setState({
@@ -27,22 +28,22 @@ export default class App extends React.Component {
 
 
 
-  render(){
+  render() {
 
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
         </View>
       )
     }
 
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
+    return (
+      <View style={{ flex: 1, paddingTop: 20 }}>
         <FlatList
           data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.id}. {item.title}, {item.releaseYear}</Text>}
-          keyExtractor={({id}, index) => id}
+          renderItem={({ item }) => <Text>{item.id}. {item.title}, {item.releaseYear}</Text>}
+          keyExtractor={({ id }, index) => id}
         />
       </View>
     );
--- requests.js
+++ requests.js
@@ -1,6 +1,7 @@
 class Requests {
   static host = "https://geodb.thecodinglab.ch/";
   static version = "v1/";
+  static address = this.host+this.version;
 
   async getData(url) {
     const response = await fetch(url);
@@ -23,81 +24,122 @@ class Requests {
     });
   }
 
+  postJsonData(url = ``, token) {
+    return fetch(url, {
+      method: "GET", // GET(default), POST, PUT, DELETE, etc.
+      headers: {
+        "X-Auth-Token": token
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+  }
+
   notNull(data) {
     return data !== null && data !== "";
   }
+
+  //01_User Account Control
+  //https://documenter.getpostman.com/view/5848189/Rzfni6B1
+
+  async registerNewUser(username, email, password) {
+    if (notNull(username) && notNull(email) && notNull(password)) {
+      return await postData(`${this.address}auth/register`,
+        {
+          'username': username,
+          'email': email,
+          'password': password
+        });
+    }else{
+      throw "not all parameters set";
+    }
+  }
+
+  async loginAsUser(username, password) {
+    if (notNull(username) && notNull(password)) {
+      return await postJsonData(`${this.address}auth/login`, {
+        'username': username,
+        'password': password
+      });
+    }else{
+      throw "username or password not set";
+    }
+  }
+  //not sure if it has to be implemented
+  async getAccountDetails() { }
+
+  async getOwnCapabilities() { }
+
+  async forgotPassword(username) {
+    if (notNull(username)) {
+      return await postJsonData(`${this.address}/user/me/forgot-password`, {
+        "username": username
+      });
+    }else{
+      throw "username not set";
+    }
+  }
+
+
 
   //02_Discticts
   //https://documenter.getpostman.com/view/5848189/Rzfni6B2
 
   async getAllDistricts() {
-    return getData(host + version + "/districts");
+    return await getData(`${address}districts/`);
   }
 
   async queryDistricts(districtname, zipcode) {
     if (notNull(districtname) && notNull(zipcode))
-      return getData(
-        host +
-          version +
-          "/districts/?name?=" +
-          districtname +
-          "&zip_code?=" +
-          zipcode
+      return await getData(`${this.address}districts/?name?=${districtname}&zip_code?=${zipcode}`
       );
   }
 
   async getDistrictViaId(districtId) {
-    if (notNull(districtId)) return getData(host + "/v1/districts/" + districtId);
+    if (notNull(districtId)) return await getData(`${this.address}districts/${districtId}`);
   }
 
   //03_Routes
   //https://documenter.getpostman.com/view/5848189/Rzfni6B3
-  
-  async queryRoutes(name, approxtime) {
+
+  async queryRoutes(routename, approxtime) {
     if (notNull(approxtime)) {
       if (notNull(name)) {
-        return getData(
-          host +
-            version +
-            "/routes/?" +
-            name +
-            "=routename&approx_time=" +
-            approxtime
-        );
+        return await getData(`${this.address}routes/?name=${routename}&approx_time=${approxtime}`);
       } else {
-        return getData(host + version + "/routes/?approx_time=" + approxtime);
+        return await getData(`${this.address}routes/?approx_time=${approxtime}`);
       }
     }
   }
 
   async getRoutesForDistrict(discrictId) {
     if (notNull(districtId))
-      return getData(host + version + "/districts/" + discrictId + "/routes");
+      return await getData(`${this.address}districts/${discrictId}/routes`);
   }
 
   async getRouteViaID(routeId) {
-    if (notNull(routeId)) return getData(host + version + "/routes/" + routeId);
+    if (notNull(routeId)) return await getData(`${this.address}routes/${routeId}`);
   }
 
   //04_Locations
   //https://documenter.getpostman.com/view/5848189/Rzfni6B4
-  
+
   async queryLocation(name) {
     if (notNull(name))
-      return getData(host + version + "/locations/?name=" + name);
+      return await getData(`${this.address}locations/?name=${name}`);
   }
 
   async getLocationsForRoute(routeId) {
     if (notNull(routeId))
-      return getData(host + version + "routes/" + routeId + "/locations");
+      return await getData(`${this.address}routes/${routeId}/locations`);
   }
 
   async getLocatoinViaId(locationId) {
-    if (notNull(locationId)) return getData(host + version + "locations/" + locationId);
+    if (notNull(locationId)) return await getData(`${this.address}locations/${locationId}`);
   }
 
-  async getLocationsForDistrict(districtId){
-    if (notNull(districtId)) return getData(host + version + "districts/" + districtId+"/locations");
+  async getLocationsForDistrict(districtId) {
+    if (notNull(districtId)) return await getData(`${this.address}districts/${districtId}/locations`);
   }
 }
 
